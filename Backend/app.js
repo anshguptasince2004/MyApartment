b6b8fadf1cf48adf7fb398a45d3cfc5a669f27f1
Ansh Gupta <anshgupta3221@gmail.com>
2025-10-02 01:55:19 +0530

    added update Due amount option for vacant & occupied flats

--- Backend/app.js
+++ Backend/app.js
@@ -18,6 +18,39 @@ db.once("open", () => {
 
 app.use(express.json());
 
+
+app.patch('/home/updateDue', async (req, res) => {
+    try {
+        const { FlatNo } = req.body;
+        await FlatOwners.updateMany(
+            { FlatNo: { $in: FlatNo }, status: 'occupied' },
+            {
+                $inc: { DueAmount: 800 }
+            });
+        await FlatOwners.updateMany(
+            { FlatNo: { $in: FlatNo }, status: 'vacant' },
+            {
+                $inc: { DueAmount: 600 }
+            });
+        res.send({ message: 'Due amounts updated successfully' });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.patch('/home/status/:flatNo', async (req, res) => {
+    const { flatNo } = (req.params);
+    const newStatus = req.body.status;
+    const owner = await FlatOwners.findOneAndUpdate({ FlatNo: Number(flatNo) }, { status: newStatus }, { new: true });
+    if (!owner) {
+        return res.status(404).send('Flat owner not found');
+    }
+    res.send(owner);
+});
+
+
 app.patch('/home/:flatNo', async (req, res) => {
     const { flatNo } = (req.params);
     const newDueAmount = req.body.DueAmount;
